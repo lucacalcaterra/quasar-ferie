e129868eb437e177d7ce73f447af3381f1ac98ba
Luca Calcaterra <calcaterra.luca@gmail.com>
2023-07-19 19:37:00 +0200

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-calendar-month
+      ref="calendar"
+      v-model="selectedDate"
+      :day-min-height="80"
+      animated
+      bordered
+      locale="it"
+      @change="onChange"
+      @moved="onMoved"
+      @click-date="onClickDate"
+      @click-day="onClickDay"
+      @click-workweek="onClickWorkweek"
+      @click-head-workweek="onClickHeadWorkweek"
+      @click-head-day="onClickHeadDay"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
